86d9c96870b4cec5f0f7398c00e98766e7912849
BaoQuocZero <130838918+BaoQuocZero@users.noreply.github.com>
2024-03-21 21:07:18 +0700

    Form Login - Dàn Layout với Bootstrap 5

--- jwtFronEnd/src/components/Login/Login.js
+++ jwtFronEnd/src/components/Login/Login.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import './Login.scss';
 
 const Login = () => {
     return (
-        <div className='login-container'>
-            login
-            <button className='btn btn-primary'>test</button>
+        <div className='login-container mt-3'>
+            <div className='container'>
+                <div className='row'>
+                    <div className='content-left col-7'>
+                        <div className='brand'>
+                            Hoi dan it
+                        </div>
+                        <div className='detail'>
+                            Ẻitnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
+                        </div>
+                    </div>
+                    <div className='content-right col-5 d-flex flex-column gap-3 py-3'>
+                        <input type='text' className='form-control' placeholder='Email address or phone number' />
+                        <input type='password' className='form-control' placeholder='Password' />
+                        <button className='btn btn-primary'>Login</button>
+                        <span className='text-center'>Fogotten password?</span>
+                        <hr />
+                        <div className='text-center'>
+                            <button className='btn btn-success'>Create new accourt</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
