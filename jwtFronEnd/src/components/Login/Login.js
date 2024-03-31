1e866299e40e5b65515ef2c2356968bf174d36a2
BaoQuocZero <130838918+BaoQuocZero@users.noreply.github.com>
2024-03-31 22:17:27 +0700

    38_83

--- jwtFronEnd/src/components/Login/Login.js
+++ jwtFronEnd/src/components/Login/Login.js
@@ -3,24 +3,30 @@ import './Login.scss';
 
 const Login = () => {
     return (
-        <div className='login-container mt-3'>
+        <div className='login-container '>
             <div className='container'>
-                <div className='row'>
+                <div className='row px-3 px-sm-0'>
 
-                    <div className='content-left col-7 d-none d-sm-block'>
+                    <div className='content-left col-12 d-none col-xl-7 d-xl-block'>
                         <div className='brand'>
                             Hoi dan it
                         </div>
                         <div className='detail'>
-                            Mọi khóa học trên Udemy, mình sẽ không pubic mà chuyển qua private, bạn nào quan tâm về khóa Fullstack JWT (json web token), Node.js và React thì:
+                            Đây là cái màn hình đang nhập siêu cấp cùi bấp của hỏi dân it
                         </div>
                     </div>
 
-                    <div className='content-right col-10 col-sm-5 d-flex flex-column gap-3 py-3'>
+                    <div className='content-right col-12 col-sm-5 d-flex flex-column gap-3 py-3'>
+                        <div className='brand d-sm-none'>
+                            Hoi dan it
+                        </div>
+
                         <input type='text' className='form-control' placeholder='Email address or phone number' />
                         <input type='password' className='form-control' placeholder='Password' />
                         <button className='btn btn-primary'>Login</button>
-                        <span className='text-center'>Fogotten password?</span>
+                        <span className='text-center'>
+                            <a className='fogot-password' href='#'>Fogotten password?</a>
+                        </span>
                         <hr />
                         <div className='text-center'>
                             <button className='btn btn-success'>Create new accourt</button>
